8f71d2e295b70555cca99478ce6ac4e05e34cf6c
TasukuY <alohaty81@gmail.com>
2022-03-16 12:45:14 -0600

    added prefix for the endpoint

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../public")));
-app.use(express.static(path.join(__dirname, "../img")));
+app.use("/img", express.static(path.join(__dirname, "../img")));
 // app.use("/styles", express.static(path.join(__dirname, "../public/index.css")));
 
 // app.get("/", function(req, res){
