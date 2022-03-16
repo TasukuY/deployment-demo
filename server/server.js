8d9fecfd307ccaadf9aa72c7543869b46e6d3bdb
TasukuY <alohaty81@gmail.com>
2022-03-16 12:29:03 -0600

    added express.static

--- server/server.js
+++ server/server.js
@@ -7,17 +7,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "../public"));
-});
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
-app.get("/styles", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.css"))
-});
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname, "../public"));
+// });
+
+// app.get("/styles", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/index.css"))
+// });
 
 const port = process.env.PORT || 4005;
 
